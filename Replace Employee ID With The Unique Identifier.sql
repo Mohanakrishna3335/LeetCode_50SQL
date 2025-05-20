1902ab1f637f1d6ff0c0884827d06daf297e76bd
Mohanakrishna3335 <54344490+Mohanakrishna3335@users.noreply.github.com>
2025-05-20 17:22:33 -0500

    Create Replace Employee ID With The Unique Identifier.sql

---
+++ Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,24 @@
+/*Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.*/
+select e.name,u.unique_id from Employees e left join EmployeeUNI u
+on e.id=u.id
